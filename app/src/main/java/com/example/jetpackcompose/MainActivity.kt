f1ce6447e006f9780dec59421f157fc3c3e6c6bb
rohan5576 <lodhirohan138@gmail.com>
2023-06-28 21:55:54 +0530

    Compose with Text and Preview Features.

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackcompose
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,13 +11,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
 import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Text(text = "Let's start Jetpack Compose.")
     }
 }
-
+    @Preview(name = "SampleApp", device = "spec:width=1080px,height=2340px,dpi=440,isRound=true",
+        showBackground = true, showSystemUi = true, backgroundColor = 0xFF673AB7,
+        uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL
+    )
+    @Composable
+    fun sampleText(text:String){
+        // Text is used to print the text.
+        Text(text)
+    }
 }

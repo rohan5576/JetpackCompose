077559a9b2892477ed615d5fa958eec0e947b3ac
rohan5576 <lodhirohan138@gmail.com>
2023-06-29 23:08:31 +0530

    Added ListViewItem UI.

---
+++ app/src/main/java/com/example/jetpackcompose/ListItemViewActivity.kt
@@ -0,0 +1,56 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+class ListItemViewActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+
+     @Preview(showSystemUi = true)
+     @Composable
+     private fun listViewItem(){
+         Column(Modifier.padding(10.dp)) {
+             Row(Modifier.padding(10.dp),) {
+                 Image(painter = painterResource(id = R.drawable.baseline_3g_mobiledata_24),
+                     contentDescription ="" )
+                 Column(Modifier.padding(5.dp)) {
+                     Text(text = "Square University")
+                     Text(text = "Senior Software Engineer")
+                 }
+             }
+             Row(Modifier.padding(10.dp)) {
+                 Image(painter = painterResource(id = R.drawable.baseline_3g_mobiledata_24),
+                     contentDescription ="" )
+                 Column(Modifier.padding(5.dp)) {
+                     Text(text = "Square University")
+                     Text(text = "Senior Software Engineer")
+                 }
+             }
+             Row(Modifier.padding(10.dp)) {
+                 Image(painter = painterResource(id = R.drawable.baseline_3g_mobiledata_24),
+                     contentDescription ="" )
+                 Column(Modifier.padding(5.dp)) {
+                     Text(text = "Square University")
+                     Text(text = "Senior Software Engineer")
+                 }
+             }
+         }
+
+     }
+}
